f90c809b2935c6fa249738e206cf209375ca5bf6
musha <mushagh@gmail.com>
2017-09-12 19:41:38 +0800

    remove document.ready function

--- js/myjs.js
+++ js/myjs.js
@@ -1,37 +1,35 @@
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
+$('[data-toggle="tooltip"]').tooltip();
 
-  function play(){
-      var audio = document.getElementById("azan2");
-      audio.play();
-  }
+function play(){
+    var audio = document.getElementById("azan2");
+    audio.play();
+}
 
-  function stop(){
-      var audio = document.getElementById("azan2");
-      audio.pause();
-  }
+function stop(){
+    var audio = document.getElementById("azan2");
+    audio.pause();
+}
 
-  function azanSubuh(){
-      var audio = document.getElementById("azan");
-      audio.play();
-  }
+function azanSubuh(){
+    var audio = document.getElementById("azan");
+    audio.play();
+}
 
-  function maxWin(){
-      var el = document.documentElement, rfs = // for newer Webkit and Firefox
-         el.requestFullScreen
-      || el.webkitRequestFullScreen
-      || el.mozRequestFullScreen
-      || el.msRequestFullScreen
-  ;
-      if(typeof rfs!="undefined" && rfs){
-          rfs.call(el);
-      } else if(typeof window.ActiveXObject!="undefined"){
+function maxWin(){
+    var el = document.documentElement, rfs = // for newer Webkit and Firefox
+       el.requestFullScreen
+    || el.webkitRequestFullScreen
+    || el.mozRequestFullScreen
+    || el.msRequestFullScreen
+;
+    if(typeof rfs!="undefined" && rfs){
+        rfs.call(el);
+    } else if(typeof window.ActiveXObject!="undefined"){
 
-      // for Internet Explorer
-      var wscript = new ActiveXObject("WScript.Shell");
-      if (wscript!=null) {
-          wscript.SendKeys("{F11}");
-      }
-      }
-  }
-});
+    // for Internet Explorer
+    var wscript = new ActiveXObject("WScript.Shell");
+    if (wscript!=null) {
+        wscript.SendKeys("{F11}");
+    }
+    }
+}
